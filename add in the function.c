f4dfc0f806c8b56e77f6fe84877e0372b6142ebb
A.HARIPRIYA <60725880+priya4-hari@users.noreply.github.com>
2021-01-18 13:42:25 +0530

    Add files via upload

---
+++ add in the function.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()   
+{
+    int add(int,int); 
+	   int a=7,b=13;    
+	   printf("%d",add(add(a,b),     add(a,b))); 
+	      return 0;
+		     }  
+			  int add(a,b)  
+			   int a,b; 
+			     {
+				     return(a+b);  
+					  }	
